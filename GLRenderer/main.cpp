fb9f1777a1866faf9f796ce4532d4b92ef526e1e
CHINESE JIGE <836613859@qq.com>
2017-11-28 23:29:10 +0800

    scrshot

--- GLRenderer/main.cpp
+++ GLRenderer/main.cpp
@@ -11,6 +11,9 @@ static float angle=0.0f;
 void Render();
 void Init();
 void Idle();
+//void Keyboard(unsigned char key, int x, int y);
+//void Mouse(int button, int state, int x, int y);
+void InputProcess();
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +22,8 @@ int main(int argc, char *argv[])
 	renderer.SetDisplayFunc(Render);
 
 	renderer.SetIdleFunc(Idle);
+
+	//renderer.SetKeyboardFunc(Keyboard);
 
 	Init();
 
@@ -39,7 +44,7 @@ void Init()
 	mesh1.LoadFile_OBJ("../media/earth.obj");
 	//mesh1.CreateBox(10.0f, 10.0f, 10.0f, 3, 3, 3);
 	mesh1.SetTexture(&diffuseMap);
-	cam.SetPosition(1.0f, 0, 0);
+	cam.SetPosition(100.0f, 0, 0);
 	cam.SetLookAt(0, 0, 0);
 	cam.SetViewAngle(60.0f,1.3333f);
 
@@ -48,6 +53,7 @@ void Init()
 void Render()
 {
 	renderer.Clear();
+	InputProcess();
 	renderer.SetTargetMesh(&mesh1);
 	renderer.SetCamera(&cam);
 	renderer.Render();
@@ -55,9 +61,43 @@ void Render()
 
 void Idle()
 {
-	static float angle = 0.0f;
-	angle += 0.0005f;
-	if (angle > Math::PI*2.0f)angle = 0.0f;
-	cam.SetPosition(70.0f * cos(angle), 50.0f, 70.0f * sin(angle));
+	//static float angle = 0.0f;
+	//angle += 0.0005f;
+	//if (angle > Math::PI*2.0f)angle = 0.0f;
+	//cam.SetPosition(70.0f * cos(angle), 50.0f, 70.0f * sin(angle));
 	renderer.Present();
+}
+
+void InputProcess()
+{
+	static int frameCount = 0;
+	static POINT prevPos = { 0.0,0.0 };
+	POINT pos;
+	GetCursorPos(&pos);
+
+	//MOUSE
+	if (frameCount != 0)
+	{
+		int deltaX = pos.x - prevPos.x;
+		int deltaY = pos.y - prevPos.y;
+		cam.RotateY_Yaw(float(deltaX) / 100.0f);
+		cam.RotateX_Pitch(float(deltaY) / 100.0f);
+		prevPos = pos;
+	}
+	else
+	{
+		//init of prevPos
+		prevPos = pos;
+	}
+
+	//KEYBOARD
+#define IS_KEY_DOWN(key) GetAsyncKeyState(key) & 0x8000
+	if (IS_KEY_DOWN('W'))cam.fps_MoveForward(0.005f, false);
+	if (IS_KEY_DOWN('A'))cam.fps_MoveRight(-0.005f, false);
+	if (IS_KEY_DOWN('S'))cam.fps_MoveForward(-0.005f, false);
+	if (IS_KEY_DOWN('D'))cam.fps_MoveRight(0.005f, false);
+	if (IS_KEY_DOWN(VK_SPACE))cam.fps_MoveUp(0.005f);
+	if (IS_KEY_DOWN(VK_LCONTROL))cam.fps_MoveUp(-0.005f);
+
+	frameCount++;
 }
